1d39926473230a47b998fad0ef45d57e8d9110d2
ImanullahSyawwaluddin <nullah1810@gmail.com>
2025-10-09 10:47:40 +0700

    Update koneksi.php

--- tugasphp/koneksi.php
+++ tugasphp/koneksi.php
@@ -2,7 +2,7 @@
 $host = "tugasinformanics-informatikamanics.l.aivencloud.com";
 $user = "avnadmin";
 $pass = "AVNS_82aKeks69pY3oCwm-Q2";
-$db   = "dbtugasreal";
+$db   = "if0_39851793_db_tugas";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
